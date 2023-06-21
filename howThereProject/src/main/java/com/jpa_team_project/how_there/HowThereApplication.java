fe16b03a0e63a568fa798c6053b7a4d5e01460d0
new-coding-kjk <kjk9104@gmail.com>
2023-06-21 16:17:22 +0900

    초기설정

---
+++ howThereProject/src/main/java/com/jpa_team_project/how_there/HowThereApplication.java
@@ -0,0 +1,13 @@
+package com.jpa_team_project.how_there;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowThereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowThereApplication.class, args);
+    }
+
+}
---
+++ howThereProject/src/test/java/com/jpa_team_project/how_there/HowThereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa_team_project.how_there;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowThereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
