393cea121cea3e049515c8301d601666cdef97e3
dev-zinc <zchan1325@gmail.com>
2023-06-24 04:26:25 +0900

    c

--- howThereProject/src/main/java/com/howthere/app/HowThereApplication.java
+++ howThereProject/src/main/java/com/howthere/app/HowThereApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HowThereApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(HowThereApplication.class, args);
     }
