0f381067cc64b0dcfefc028e39ec6b6b9cafa46c
dongJ <alalal2234@gmail.com>
2023-06-30 20:59:41 +0900

    렌터카 사이트 작업 중

---
+++ howThereProject/src/main/java/com/howthere/app/controller/ProgramController.java
@@ -0,0 +1,19 @@
+package com.howthere.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/program")
+public class ProgramController {
+
+    @GetMapping("/list")
+    public ModelAndView programMainPage(HttpServletRequest req, ModelAndView mv) {
+        mv.setViewName("list_t");
+        return mv;
+    }
+}
