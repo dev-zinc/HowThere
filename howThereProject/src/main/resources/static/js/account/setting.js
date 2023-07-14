ea1fe6c6aea1affc241508a805082744592f6aae
lc0709 <3dollee@gmail.com>
2023-07-15 01:09:52 +0900

    setting 페이지 js까지 완료

--- howThereProject/src/main/resources/static/js/account/setting.js
+++ howThereProject/src/main/resources/static/js/account/setting.js
@@ -1,14 +1,40 @@
 const $modifyButtons = $(".info-modify button");
 const $rows = $(".info-block");
+const $nameModify = $("#name_modify");
+const $emailModify = $("#email_modify");
+const $phoneNumbersModify = $("#phone_numbers_modify");
 
 let isClicked = new Array($rows.size).fill(false);
+let modifyInputs = [$nameModify, $emailModify, $phoneNumbersModify];
+
+$nameModify.hide();
+$emailModify.hide();
+$phoneNumbersModify.hide();
 
 $modifyButtons.each((i, button) => {
     $(button).on('click', function (e) {
+        let showColor;
+        let text;
+
         e.preventDefault()
-        let showColor = isClicked[i] ? "#222222" : "#DDDDDD";
+        if(isClicked.reduce((prev, next) => prev || next) && !isClicked[i]) return;
+
+        isClicked[i] = !isClicked[i];
+
+        if(isClicked[i]) {
+            showColor = "#DDDDDD";
+            text = "취소";
+            modifyInputs[i].show();
+        } else {
+            showColor = "#222222";
+            text = "수정";
+            modifyInputs[i].hide();
+        }
+
+        $(this).text(text);
         $rows.filter(j => i != j).each((_, other) => {
-            $(other).color = showColor;
+            $(other).css("color", showColor);
+            $(other).find(".info .info-text").css("color", showColor);
         });
     });
 });
