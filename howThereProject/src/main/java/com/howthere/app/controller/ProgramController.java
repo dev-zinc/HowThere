9dc9b7d7a7406fb234c69364a2c49cf074319530
dongJ <alalal2234@gmail.com>
2023-07-19 00:51:04 +0900

    프로그램 상세 페이지 작업 중

--- howThereProject/src/main/java/com/howthere/app/controller/ProgramController.java
+++ howThereProject/src/main/java/com/howthere/app/controller/ProgramController.java
@@ -2,6 +2,7 @@ package com.howthere.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,8 +13,14 @@ import javax.servlet.http.HttpServletRequest;
 public class ProgramController {
 
     @GetMapping("/list")
-    public ModelAndView testPage(HttpServletRequest req, ModelAndView mv) {
+    public ModelAndView list(HttpServletRequest req, ModelAndView mv) {
         mv.setViewName("program/list");
+        return mv;
+    }
+
+    @GetMapping("/detail")
+    public ModelAndView detail(HttpServletRequest req, ModelAndView mv) {
+        mv.setViewName("program/detail");
         return mv;
     }
 }
