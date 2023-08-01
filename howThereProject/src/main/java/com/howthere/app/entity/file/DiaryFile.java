f7ac1e7e19ca625057062bbea7a8877eafe469b1
heesu0_0 <ss00.coder@gmail.com>
2023-08-01 10:14:29 +0900

    Revert "0801 diaryFileEntity"
    This reverts commit 54c4589945412b811b7dd74645b7af164db044c7.

--- howThereProject/src/main/java/com/howthere/app/entity/file/DiaryFile.java
+++ howThereProject/src/main/java/com/howthere/app/entity/file/DiaryFile.java
@@ -1,21 +1,18 @@
 package com.howthere.app.entity.file;
 
-import com.howthere.app.entity.Diary;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "TBL_DIARY_FILE")
+@Table(name = "TBL_ANNOUNCE_FILE")
 @Getter @ToString(callSuper = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class DiaryFile extends FileEntity {
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Diary diary;
+    //@ManyToOne(join, fetch = FetchType.LAZY)
+    //private Diary diary;
 }
