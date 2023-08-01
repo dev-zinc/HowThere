54c4589945412b811b7dd74645b7af164db044c7
heesu0_0 <ss00.coder@gmail.com>
2023-08-01 09:59:49 +0900

    0801 diaryFileEntity

--- howThereProject/src/main/java/com/howthere/app/entity/file/DiaryFile.java
+++ howThereProject/src/main/java/com/howthere/app/entity/file/DiaryFile.java
@@ -1,18 +1,21 @@
 package com.howthere.app.entity.file;
 
+import com.howthere.app.entity.Diary;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "TBL_ANNOUNCE_FILE")
+@Table(name = "TBL_DIARY_FILE")
 @Getter @ToString(callSuper = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class DiaryFile extends FileEntity {
-    //@ManyToOne(join, fetch = FetchType.LAZY)
-    //private Diary diary;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Diary diary;
 }
