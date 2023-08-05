ca6caf0056e51e08bb6b4f0bd72a0b382dfc761d
dev-zinc <zchan1325@gmail.com>
2023-08-05 13:01:40 +0900

    Program House 연관관계 추가

--- howThereProject/src/main/java/com/howthere/app/domain/program/ProgramDTO.java
+++ howThereProject/src/main/java/com/howthere/app/domain/program/ProgramDTO.java
@@ -1,13 +1,33 @@
 package com.howthere.app.domain.program;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 @Data
 @NoArgsConstructor
 public class ProgramDTO {
     private Long id;
+    private Long memberId;
+    @DateTimeFormat(pattern = "yyyy.MM.dd")
+    private LocalDate createdDate;
+    private String programAddress;
+    private String programName;
+    private String programContent;
+    private boolean verified;
 
+    @Builder
+    public ProgramDTO(Long memberId, LocalDate createdDate, String programAddress, String programName, String programContent, boolean verified) {
+        this.memberId = memberId;
+        this.createdDate = createdDate;
+        this.programAddress = programAddress;
+        this.programName = programName;
+        this.programContent = programContent;
+        this.verified = verified;
+    }
 }
--- howThereProject/src/main/java/com/howthere/app/repository/program/ProgramQueryDSLImpl.java
+++ howThereProject/src/main/java/com/howthere/app/repository/program/ProgramQueryDSLImpl.java
@@ -1,6 +1,8 @@
 package com.howthere.app.repository.program;
 
-import com.howthere.app.entity.Program;
+import com.howthere.app.domain.program.ProgramDTO;
+import com.howthere.app.entity.program.Program;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -9,16 +11,20 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-import static com.howthere.app.entity.QProgram.program;
+import static com.howthere.app.entity.program.QProgram.program;
 
 @RequiredArgsConstructor
 public class ProgramQueryDSLImpl implements ProgramQueryDSL {
     private JPAQueryFactory query;
 
     @Override
-    public Page<Program> findAllWithLimit(Pageable pageable, String keyword) {
-        final List<Program> programs = query.select(program).from(program).where(program.programName.contains(keyword)).fetch();
+    public Page<ProgramDTO> findAllWithLimit(Pageable pageable, String keyword) {
+        final List<ProgramDTO> programDTOs = query.select(
+                Projections.fields(ProgramDTO.class,
+                    program.id
+                )
+        ).from(program).where(program.programName.contains(keyword)).fetch();
         Long count = query.select(program.count()).from(program).fetchOne();
-        return new PageImpl<>(programs, pageable, count == null ? 0 : count);
+        return new PageImpl<>(programDTOs, pageable, count == null ? 0 : count);
     }
 }
