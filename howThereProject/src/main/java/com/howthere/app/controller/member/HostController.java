a6422f6ba3b60d1c5145cee7bd5b19a1c85cf920
dongJ <alalal2234@gmail.com>
2023-08-12 04:41:34 +0900

    호스트 하우스 등록 구현 작업 중
    - 파일 리스트 넘기기 완
    - 파라미터 읽어오기

--- howThereProject/src/main/java/com/howthere/app/controller/member/HostController.java
+++ howThereProject/src/main/java/com/howthere/app/controller/member/HostController.java
@@ -1,25 +1,103 @@
 package com.howthere.app.controller.member;
 
+import com.howthere.app.service.member.HostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnailator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/host/*")
 @Slf4j
 @RequiredArgsConstructor
 public class HostController {
-//    http://localhost:10000/host/write
+
+    private final HostService hostService;
+
+    @Value("${file-root}")
+    private String FILE_ROOT;
+
+    //    http://localhost:10000/host/write
     @GetMapping("write")
-    public void write(){;}
+    public void write() {
 
-//    http://localhost:10000/host/inn
+    }
+
+    //    http://localhost:10000/host/inn
     @GetMapping("inn")
-    public void inn(){;}
+    public void inn() {
 
-//    http://localhost:10000/host/hosting
+    }
+
+    //    http://localhost:10000/host/hosting
     @GetMapping("hosting")
-    public void hosting(){;}
+    public void hosting() {
+        ;
+    }
+
+    @PostMapping("write")
+    public void registerHouse(MultipartHttpServletRequest req) {
+        final String houseName = req.getParameter("houseName");
+        final String address = req.getParameter("address");
+        final String addressDetail = req.getParameter("addressDetail");
+        final String summernote = req.getParameter("content");
+        final String guestCnt = req.getParameter("maxGuestCnt");
+        final String petCnt = req.getParameter("maxPetCnt");
+        req.getFileNames().forEachRemaining(System.out::println);
+        final List<MultipartFile> files = req.getFiles("files");
+        files.forEach(v -> {
+            System.out.println(v.getOriginalFilename());
+        });
+        final List<MultipartFile> images = req.getFiles("houseImg");
+        images.forEach(v -> {
+            System.out.println(v.getOriginalFilename());
+        });
+    }
+
+    @PostMapping("upload")
+    public String upload(@RequestParam("uploadFile") List<MultipartFile> uploadFiles) {
+        uploadFiles.forEach(v -> {
+            System.out.println(v.getOriginalFilename());
+        });
+//        String path = FILE_ROOT;
+//        List<String> saveFileNames = new ArrayList<>();
+//        File dir = new File(path);
+//        try {
+//            if (!dir.exists()) {
+//                dir.mkdirs();
+//            }
+//
+//            for (MultipartFile multipartFile : uploadFiles) {
+//                String saveFileName = UUID.randomUUID().toString() + "_" + multipartFile.getOriginalFilename();
+//                multipartFile.transferTo(new File(path, saveFileName));
+//
+//                if (multipartFile.getContentType().startsWith("image")) {
+//                    try (FileOutputStream out = new FileOutputStream(new File(path, "t_" + saveFileName))) {
+//                        Thumbnailator.createThumbnail(multipartFile.getInputStream(), out, 100, 100);
+//                    } catch (FileNotFoundException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return saveFileNames;
+        return "OK";
+    }
 }
--- howThereProject/src/main/resources/static/js/host/write.js
+++ howThereProject/src/main/resources/static/js/host/write.js
@@ -1,27 +1,27 @@
-function searchAddress(e){
-    if(e.keyCode == 13){
+function searchAddress(e) {
+    if (e.keyCode == 13) {
         $("#address-search-btn").trigger("click");
     }
 }
 
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+const mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
         center: new daum.maps.LatLng(37.537187, 127.005476), // 지도의 중심좌표
         level: 5 // 지도의 확대 레벨
     };
 
 //지도를 미리 생성
-var map = new daum.maps.Map(mapContainer, mapOption);
+const map = new daum.maps.Map(mapContainer, mapOption);
 //주소-좌표 변환 객체를 생성
-var geocoder = new daum.maps.services.Geocoder();
+const geocoder = new daum.maps.services.Geocoder();
 //마커를 미리 생성
-var marker = new daum.maps.Marker({
+const marker = new daum.maps.Marker({
     map: map
 });
 
 function openAddressSearchPopup() {
     new daum.Postcode({
-        oncomplete: function(data) {
+        oncomplete: function (data) {
             const {address, sido, sigungu} = data;
 
             // 주소 정보를 해당 필드에 넣는다.
@@ -32,7 +32,7 @@ function openAddressSearchPopup() {
             $(".address-wrap>div").removeClass("hidden");
 
             // 주소로 상세 정보를 검색
-            geocoder.addressSearch(address, function(results, status) {
+            geocoder.addressSearch(address, function (results, status) {
                 // 정상적으로 검색이 완료됐으면
                 if (status === daum.maps.services.Status.OK) {
 
@@ -51,10 +51,11 @@ function openAddressSearchPopup() {
             });
         }
     }).open({
-        q : $("#searchAddress").val() // 팝업 오픈 시 검색어 전달
+        q: $("#searchAddress").val() // 팝업 오픈 시 검색어 전달
     });
 }
-$(function() {
+
+$(function () {
     //여기 아래 부분
     $('#summernote').summernote({
         height: 300,                 // 에디터 높이
@@ -67,20 +68,63 @@ $(function() {
 
     $("#searchAddress").on("keyup", searchAddress);
 
-    $(".maxCount").click(function(e){
-        const {for : id, oper} = e.currentTarget.dataset;
+    $(".maxCount").click(function (e) {
+        const {for: id, oper} = e.currentTarget.dataset;
         const $target = $("#" + id);
         const orgVal = $target.text();
 
-        switch(oper){
+        switch (oper) {
             case 'minus':
-                if(orgVal == 0){// orgVal 가 0이면
+                if (orgVal == 0) {// orgVal 가 0이면
                     return;
                 }
                 $target.text(Number(orgVal) - 1);
                 break;
-            case 'plus': $target.text(Number(orgVal) + 1);
+            case 'plus':
+                $target.text(Number(orgVal) + 1);
                 break;
         }
     })
 });
+
+function registerHouse() {
+    // const form = document.querySelector("#form");
+    // const files = document.querySelector("#imgFileAddBtn").files;
+    // const guestCnt = document.querySelector("#maxGuestCnt").innerHTML;
+    // const petCnt = document.querySelector("#maxPetCnt").innerHTML;
+    // form.append(files);
+    // form.append(guestCnt);
+    // form.append(petCnt);
+    return true;
+}
+
+function submit() {
+    const form = document.querySelector("#form");
+    const files = document.querySelector("input[name='houseImg']").files;
+    const maxGuestCnt = document.querySelector("input[name='maxGuestCnt']");
+    const maxPetCnt = document.querySelector("input[name='maxPetCnt']");
+    maxGuestCnt.value = document.querySelector("#maxGuestCnt").innerHTML;
+    maxPetCnt.value = document.querySelector("#maxPetCnt").innerHTML;
+    for (let i = 0; i < files.length; i++) {
+        const image = files[i];
+        form.append(image);
+    }
+    form.append(maxGuestCnt);
+    form.append(maxPetCnt);
+    form.submit();
+}
+
+function upload(){
+    const files = document.querySelector("input[name='houseImg']").files;
+    const formData = new FormData();
+
+    for (let i = 0; i < files.length; i++) {
+        const image = files[i];
+        formData.append('uploadFile', image);
+    }
+
+    fetch("/host/upload", {
+        method: "POST",
+        body: formData
+    }).then(res => console.log(res));
+}
