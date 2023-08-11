accc1a8940344374c78b039dbf7c2c3e3d284892
dongJ <alalal2234@gmail.com>
2023-08-11 22:22:17 +0900

    프로그램 리스트 페이지 맵 제거

--- howThereProject/src/main/java/com/howthere/app/config/ConstantPool.java
+++ howThereProject/src/main/java/com/howthere/app/config/ConstantPool.java
@@ -1,5 +1,14 @@
 package com.howthere.app.config;
 
 public class ConstantPool {
-    static final public String FILE_ROOT = "C:/upload/";
+    private static final String WINDOW_FILE_ROOT = "C:/upload/";
+    private static final String LINUX_FILE_ROOT = "/upload/";
+
+    public String getFileRootPath() {
+        final String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            return WINDOW_FILE_ROOT;
+        } else
+            return LINUX_FILE_ROOT;
+    }
 }
