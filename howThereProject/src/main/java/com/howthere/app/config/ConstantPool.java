27975578354101508942ce7bffb29cb7a400e95a
dongJ <alalal2234@gmail.com>
2023-08-12 11:37:33 +0900

    운영체제에 맞춰 파일경로 가져오는 스태틱 메소드 생성

--- howThereProject/src/main/java/com/howthere/app/config/ConstantPool.java
+++ howThereProject/src/main/java/com/howthere/app/config/ConstantPool.java
@@ -4,7 +4,7 @@ public class ConstantPool {
     private static final String WINDOW_FILE_ROOT = "C:/upload/";
     private static final String LINUX_FILE_ROOT = "/upload/";
 
-    public String getFileRootPath() {
+    public static String getFileRootPath() {
         final String os = System.getProperty("os.name").toLowerCase();
         if (os.contains("win")) {
             return WINDOW_FILE_ROOT;
