977bbd49dab0161665d09c0237a15f060062aeae
dongJ <alalal2234@gmail.com>
2023-08-19 10:16:24 +0900

    Format

--- howThereProject/src/main/java/com/howthere/app/controller/member/HostController.java
+++ howThereProject/src/main/java/com/howthere/app/controller/member/HostController.java
@@ -4,6 +4,8 @@ import com.howthere.app.entity.file.HouseFile;
 import com.howthere.app.entity.house.House;
 import com.howthere.app.service.file.house.HouseFileService;
 import com.howthere.app.service.house.HouseService;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -14,39 +16,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-
 @Controller
 @RequestMapping("/host/*")
 @Slf4j
 @RequiredArgsConstructor
 public class HostController {
 
-    private final HouseService houseService;
-    private final HouseFileService houseFileService;
+  private final HouseService houseService;
+  private final HouseFileService houseFileService;
 
-    //    http://localhost:10000/host/write
-    @GetMapping("write")
-    public void write() {
+  //    http://localhost:10000/host/write
+  @GetMapping("write")
+  public void write() {
 
-    }
+  }
 
-    //    http://localhost:10000/host/inn
-    @GetMapping("inn")
-    public void inn() {
-    }
+  //    http://localhost:10000/host/inn
+  @GetMapping("inn")
+  public void inn() {
+    // 1. 하우스 페이징으로 가져옴.
+    // 2. HouseFile 가져오기.
+  }
 
-    //    http://localhost:10000/host/hosting
-    @GetMapping("hosting")
-    public void hosting() {
-        ;
-    }
+  //    http://localhost:10000/host/hosting
+  @GetMapping("hosting")
+  public void hosting() {
+    ;
+  }
 
-    @PostMapping("write")
-    public RedirectView registerHouse(HttpServletRequest req, @RequestParam("thumbnail") MultipartFile thumbnail, @RequestParam("houseImg") List<MultipartFile> images) {
-        final House saved = houseService.save(req);
-        final List<HouseFile> fileList = houseFileService.registerHouse(saved, thumbnail, images);
-        return new RedirectView("/host/inn");
-    }
+  @PostMapping("write")
+  public RedirectView registerHouse(HttpServletRequest req,
+      @RequestParam("thumbnail") MultipartFile thumbnail,
+      @RequestParam("houseImg") List<MultipartFile> images) {
+    final House saved = houseService.save(req);
+    final List<HouseFile> fileList = houseFileService.registerHouse(saved, thumbnail, images);
+    return new RedirectView("/host/inn");
+  }
 }
--- howThereProject/src/main/java/com/howthere/app/service/house/HouseServiceImpl.java
+++ howThereProject/src/main/java/com/howthere/app/service/house/HouseServiceImpl.java
@@ -3,56 +3,54 @@ package com.howthere.app.service.house;
 import com.howthere.app.domain.house.HouseDTO;
 import com.howthere.app.embed.Address;
 import com.howthere.app.entity.house.House;
-import com.howthere.app.entity.member.Member;
 import com.howthere.app.repository.house.HouseRepository;
 import com.howthere.app.repository.member.MemberRepository;
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Service
 @RequiredArgsConstructor
 public class HouseServiceImpl implements HouseService {
-    private final HouseRepository houseRepository;
-    private final MemberRepository memberRepository;
 
-    @Override
-    public Page<HouseDTO> getHouses(Pageable pageable, String keyword) {
-        return houseRepository.findWithLimitAndKeyword(pageable, keyword);
-    }
+  private final HouseRepository houseRepository;
+  private final MemberRepository memberRepository;
 
-    @Override
-    public House save(HttpServletRequest req) {
-        final String houseTitle = req.getParameter("houseTitle");
-        final String address = req.getParameter("address");
-        final String addressDetail = req.getParameter("addressDetail");
-        final String houseContent = req.getParameter("content");
-        final Integer guestCnt = Integer.parseInt(req.getParameter("maxGuestCnt"));
-        final Integer petCnt = Integer.parseInt(req.getParameter("maxPetCnt"));
-        final Double lat = Double.parseDouble(req.getParameter("latitude"));
-        final Double lon = Double.parseDouble(req.getParameter("longitude"));
+  @Override
+  public Page<HouseDTO> getHouses(Pageable pageable, String keyword) {
+    return houseRepository.findWithLimitAndKeyword(pageable, keyword);
+  }
 
-        // TODO: 2023/08/19 후에 DTO 협의 후 수정
-        final Address addressEntity = Address.builder()
-                .address(address)
-                .addressDetail(addressDetail)
-                .latitude(lat)
-                .longitude(lon)
-                .build();
-//        final Member member = (Member) req.getSession().getAttribute("id");
+  @Override
+  public House save(HttpServletRequest req) {
+    final String houseTitle = req.getParameter("houseTitle");
+    final String address = req.getParameter("address");
+    final String addressDetail = req.getParameter("addressDetail");
+    final String houseContent = req.getParameter("content");
+    final Integer guestCnt = Integer.parseInt(req.getParameter("maxGuestCnt"));
+    final Integer petCnt = Integer.parseInt(req.getParameter("maxPetCnt"));
+    final Double lat = Double.parseDouble(req.getParameter("latitude"));
+    final Double lon = Double.parseDouble(req.getParameter("longitude"));
 
-        final House house = House.builder()
-                .houseTitle(houseTitle)
-                .houseContent(houseContent)
-                .houseMaxHeadCount(guestCnt)
-                .houseMaxPetCount(petCnt)
-                .address(addressEntity)
-                // TODO: 2023/08/19 멤버 가져오는 로직 수정 필요 
-                .member(memberRepository.findById(1L).get())
-                .build();
-        return houseRepository.save(house);
-    }
+    // TODO: 2023/08/19 후에 DTO 협의 후 수정
+    final Address addressEntity = Address.builder()
+        .address(address)
+        .addressDetail(addressDetail)
+        .latitude(lat)
+        .longitude(lon)
+        .build();
+
+    final House house = House.builder()
+        .houseTitle(houseTitle)
+        .houseContent(houseContent)
+        .houseMaxHeadCount(guestCnt)
+        .houseMaxPetCount(petCnt)
+        .address(addressEntity)
+        // TODO: 2023/08/19 멤버 가져오는 로직 수정 필요
+        .member(memberRepository.findById(1L).get())
+        .build();
+    return houseRepository.save(house);
+  }
 }
