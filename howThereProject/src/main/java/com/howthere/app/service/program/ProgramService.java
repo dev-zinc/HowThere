3d7345a152cba140ba809569c7d5b44aafca6c17
dongJ <alalal2234@gmail.com>
2023-08-25 23:39:40 +0900

    호스팅 등록 수정 - 프로그램 이름 설정 생각

--- howThereProject/src/main/java/com/howthere/app/service/program/ProgramService.java
+++ howThereProject/src/main/java/com/howthere/app/service/program/ProgramService.java
@@ -3,7 +3,6 @@ package com.howthere.app.service.program;
 import com.howthere.app.domain.program.ProgramDTO;
 import com.howthere.app.entity.house.House;
 import com.howthere.app.entity.program.Program;
-import java.time.LocalDateTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -15,9 +14,10 @@ public interface ProgramService {
 
     default Program toEntity(ProgramDTO dto) {
         return Program.builder()
-            .programStartDate(LocalDateTime.from(dto.getProgramStartDate()))
-            .programEndDate(LocalDateTime.from(dto.getProgramEndDate()))
-            .programName(dto.getProgramName())
+            .programStartDate(dto.getProgramStartDate())
+            .programEndDate(dto.getProgramEndDate())
+//            .programName(dto.getProgramName())
+            .programName("프로그램 네임") // TODO: 2023/08/25 프로그램 이름 설정 어떻게 해야하는지..
             .programContent(dto.getProgramContent())
             .programPrice(dto.getProgramPrice())
             .house(House.builder().id(dto.getHouseId()).build())
